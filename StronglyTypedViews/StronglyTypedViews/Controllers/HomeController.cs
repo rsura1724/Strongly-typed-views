ea014af1336f91b1115d88f7f3be81809387d425
rsura1724 <31663929+rsura1724@users.noreply.github.com>
2017-09-27 02:02:47 -0400

    Strongly typed initial commit

---
+++ StronglyTypedViews/StronglyTypedViews/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StronglyTypedViews.Models;
+
+namespace StronglyTypedViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Register r = new Register() {userID=1, userName="ben", email="abc.com", gender="female" };
+
+            return View(r);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ StronglyTypedViews/StronglyTypedViews/Models/Register.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StronglyTypedViews.Models
+{
+    public class Register
+    {
+        public int userID { get; set; }
+        public string userName { get; set; }
+        public string gender { get; set; }
+        public string  email { get; set; }
+    }
+}
